8add0137ec4e64a70cd41eb96abc2e87fd696769
EduardSzamosvari <Rebreanu8>
2021-06-06 17:19:23 +0300

    add file

---
+++ 1. SearchGoogle.py
@@ -0,0 +1,23 @@
+import time
+
+from selenium import webdriver
+driver = webdriver.Chrome(executable_path="D:\\Curs Software tester\\Softuri\chromedriver.exe")
+
+class Google():
+
+    def SearchGoogle(self):
+        driver.get("https://www.google.ro/")
+        #agree = driver.find_element_by_xpath("//div[normalize-space()='Sunt de acord']")
+        agree = driver.find_element_by_id("L2AGLb")
+        agree.click()
+        #find = driver.find_element_by_xpath("//input[@title='Căutați']")
+        find = driver.find_element_by_name("q")
+        find.send_keys("selenium")
+        find.submit()
+
+        time.sleep(10)
+
+
+search = Google()
+search.SearchGoogle()
+driver.quit()
